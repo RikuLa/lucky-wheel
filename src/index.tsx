93527ee0d4dd1cf6ae7d0984eb794382bd7fd935
Jere Vaara <jerevaara@gmail.com>
2020-11-20 20:29:22 +0200

    lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <div>moi</div>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
