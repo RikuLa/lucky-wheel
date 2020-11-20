de71f27c96c0a5b0bf5134c7dd5176ff7cc9f5c0
Jere Vaara <jerevaara@gmail.com>
2020-11-20 19:23:40 +0200

    update

---
+++ src/House/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class House extends React.PureComponent {
+  render() {
+    return <div>I am house</div>
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom'
+import { House } from './House';
 
 ReactDOM.render(
-  <div>moi</div>,
+  <House />,
   document.getElementById('root')
 );
