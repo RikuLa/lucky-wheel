f9db84f9407e7561982bb8a3d793f07b419668d9
Visa Pollari <visa.pollari@gmail.com>
2020-11-20 22:49:37 +0200

    Add resize observer

--- src/resizeBox.tsx
+++ src/resizeBox.tsx
@@ -8,7 +8,6 @@ let originalY = 0;
 let originalMouseX = 0;
 let originalMouseY = 0;
 
-
 export default function ResizeBox() {
   //const [movingCorner, setCorner] = useState(null)
   useEffect(() => {
@@ -17,9 +16,7 @@ export default function ResizeBox() {
     for (let i = 0; i < corners.length; i += 1) {
       const corner = corners[i];
       const resize = (e) => {
-        console.log(e.target.classList, corner)
-        // const corner = e.target;
-        if (corner.classList.contains('bottom-right')) {
+        if (corner.classList.contains("bottom-right")) {
           const width = originalWidth + (e.pageX - originalMouseX);
           const height = originalHeight + (e.pageY - originalMouseY);
           if (width > minimumSize) {
@@ -28,7 +25,7 @@ export default function ResizeBox() {
           if (height > minimumSize) {
             box.style.height = `${height}px`;
           }
-        } else if (corner.classList.contains('bottom-left')) {
+        } else if (corner.classList.contains("bottom-left")) {
           const height = originalHeight + (e.pageY - originalMouseY);
           const width = originalWidth - (e.pageX - originalMouseX);
           if (height > minimumSize) {
@@ -38,7 +35,7 @@ export default function ResizeBox() {
             box.style.width = `${width}px`;
             box.style.left = `${originalX + (e.pageX - originalMouseX)}px`;
           }
-        } else if (corner.classList.contains('top-right')) {
+        } else if (corner.classList.contains("top-right")) {
           const width = originalWidth + (e.pageX - originalMouseX);
           const height = originalHeight - (e.pageY - originalMouseY);
           if (width > minimumSize) {
@@ -48,7 +45,7 @@ export default function ResizeBox() {
             box.style.height = `${height}px`;
             box.style.top = `${originalY + (e.pageY - originalMouseY)}px`;
           }
-        } else if (corner.classList.contains('top-left'){
+        } else if (corner.classList.contains("top-left")) {
           const width = originalWidth - (e.pageX - originalMouseX);
           const height = originalHeight - (e.pageY - originalMouseY);
           if (width > minimumSize) {
@@ -60,36 +57,54 @@ export default function ResizeBox() {
             box.style.top = `${originalY + (e.pageY - originalMouseY)}px`;
           }
         }
-      }
+      };
       const stopResize = () => {
-        console.log("soppi")
-        window.removeEventListener('mousemove', resize);
-      }
+        console.log("soppi");
+        window.removeEventListener("mousemove", resize);
+      };
       const mouseEvent = (e) => {
         e.preventDefault();
-        originalWidth =
-          parseFloat(
-            getComputedStyle(box, null)
-              .getPropertyValue("width")
-              .replace("px", "")
-          )
-        originalHeight =
-          parseFloat(
-            getComputedStyle(box, null)
-              .getPropertyValue("height")
-              .replace("px", "")
-          )
-        originalX = box.getBoundingClientRect().left
-        originalY = box.getBoundingClientRect().top
-        originalMouseX = e.pageX
-        originalMouseY = e.pageY
-        console.log("dododoo", e.target)
+        originalWidth = parseFloat(
+          getComputedStyle(box, null)
+            .getPropertyValue("width")
+            .replace("px", "")
+        );
+        originalHeight = parseFloat(
+          getComputedStyle(box, null)
+            .getPropertyValue("height")
+            .replace("px", "")
+        );
+        originalX = box.getBoundingClientRect().left;
+        originalY = box.getBoundingClientRect().top;
+        originalMouseX = e.pageX;
+        originalMouseY = e.pageY;
+        console.log("dododoo", e.target);
         window.addEventListener("mousemove", resize);
         window.addEventListener("mouseup", stopResize);
       };
-      corner.addEventListener('mousedown', mouseEvent);
+      corner.addEventListener("mousedown", mouseEvent);
     }
-  
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      console.log("Size changed");
+      entries.forEach((entry) => {
+        const cr = entry.contentRect;
+        /*
+        console.log("box:", entry.target);
+        console.log(`box size: ${cr.width}px x ${cr.height}px`);
+        console.log(`box padding: ${cr.top}px ; ${cr.left}px`);
+        */
+        if (cr.width > cr.height) {
+          console.log("Ländskeip");
+        } else if (cr.width < cr.height) {
+          console.log("porttrait");
+        } else {
+          console.log("boxxx");
+        }
+      });
+    });
+
+    resizeObserver.observe(box);
   }, []);
 
   return (
