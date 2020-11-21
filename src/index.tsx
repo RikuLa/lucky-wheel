42f6eaed3c23733df1f10bbd8e3a0246b567d700
Visa Pollari <visa.pollari@gmail.com>
2020-11-21 11:28:04 +0200

    Use styled components

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { WordTuner } from "./House/Rooms/WordTuner";
 // import { House } from "./House";
-// import ResizeBox from "./resizeBox";
+import ResizeBox from "./resizeBox";
 
-ReactDOM.render(<WordTuner />, document.getElementById("root"));
+ReactDOM.render(<ResizeBox />, document.getElementById("root"));
