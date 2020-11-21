e0ade06942544333ab26ec713cef09ef8e0958b1
wyozi <joonas.mertanen@gmail.com>
2020-11-21 21:24:06 +0200

    Add other tasks too

--- src/House/Spaceship.tsx
+++ src/House/Spaceship.tsx
@@ -13,10 +13,20 @@ img.src = spaceship;
 
 const updateCanvas = (canvas: HTMLCanvasElement, props: Props) => {
   const ctx = canvas.getContext("2d");
+  ctx.textAlign = "center";
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
+  ctx.fillStyle = props.roomStates.resizer.completed ? "green" : "gray";
+  ctx.fillRect(0.35 * canvas.width - 5, 0.15 * canvas.height, 10, 10);
+  ctx.fillText("Scanner", 0.35 * canvas.width, 0.15 * canvas.height);
+
+  ctx.fillStyle = props.roomStates.wordBox.completed ? "green" : "gray";
+  ctx.fillRect(0.4 * canvas.width - 5, 0.8 * canvas.height, 10, 10);
+  ctx.fillText("Word Box", 0.4 * canvas.width, 0.8 * canvas.height);
+
   ctx.fillStyle = props.roomStates.engine.completed ? "green" : "gray";
-  ctx.fillRect(0.8 * canvas.width, 0.2 * canvas.height, 10, 10);
+  ctx.fillRect(0.65 * canvas.width - 5, 0.2 * canvas.height, 10, 10);
+  ctx.fillText("Engine", 0.65 * canvas.width, 0.2 * canvas.height);
 };
 
 export default function Spaceship(props: Props) {
