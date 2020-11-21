b4021878f61b941c475d034be60e90d879bf6b1c
riku <riku.laukkanen@aalto.fi>
2020-11-21 12:56:57 +0200

    add dials and dummy send button

--- src/House/Rooms/WordTuner.tsx
+++ src/House/Rooms/WordTuner.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { MessageEmitter } from "./util/WordStream";
 import { RoomApi } from "../rooms";
+import { Silver } from "react-dial-knob";
 
 const Radio = styled.div`
   height: 500px;
   width: 500px;
-  background-color: black;
+  background-color: #111111;
   border: 1px solid gray;
   border-radius: 6px;
   padding: 20px;
+  font-family: "Courier New", Courier, monospace;
 `;
 
 const TextDisplay = styled.div`
@@ -18,18 +20,33 @@ const TextDisplay = styled.div`
   color: lightgreen;
   font-family: "Courier New", Courier, monospace;
   font-size: 50px;
-  border-radius: 2px;
+  border-radius: 4px;
   line-height: 75px;
   height: 75px;
 `;
 
-const CipherInput = styled.input`
-  margin-top: 20px;
+const KnobContainer = styled.div`
+  margin-top: 40px;
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SubmitButton = styled.div`
+  margin-top: 40px;
+  background-color: darkgreen;
+  color: lightgreen;
+  height 50px;
+  font-size: 50px;
+  width: 50%;
+  text-align: center;
+  cursor: pointer;
 `;
 
 interface State {
   message: string;
+  cipher: number;
+  channel: number;
 }
 
 export class WordTuner extends React.PureComponent<RoomApi, State> {
@@ -44,36 +61,53 @@ export class WordTuner extends React.PureComponent<RoomApi, State> {
 
   readonly state = {
     message: "",
+    cipher: 0,
+    channel: 0,
   };
 
-  private updateCipher = (foo) => {
-    this.emitter.setCipher(foo.target.value);
+  private updateCipher = (cipher: number) => {
+    this.setState({ cipher });
+    this.emitter.setCipher(cipher);
   };
 
-  private updateChannel = (foo) => {
-    this.emitter.setActiveStream(foo.target.value);
+  private updateChannel = (channel: number) => {
+    this.setState({ channel });
+    this.emitter.setActiveStream(channel);
   };
 
   render() {
     return (
       <Radio>
         <TextDisplay>{this.state.message}</TextDisplay>
-        <CipherInput
-          type={"range"}
-          min="6"
-          max="20"
-          step="1"
-          onChange={this.updateCipher}
-          value={this.emitter.getCipher}
-        />
-        <CipherInput
-          type={"range"}
-          min="0"
-          max="2"
-          step="1"
-          onChange={this.updateChannel}
-          value={this.emitter.activeChannel}
-        />
+        <KnobContainer>
+          <Silver
+            diameter={180}
+            min={6}
+            max={20}
+            step={1}
+            onValueChange={this.updateCipher}
+            value={this.state.cipher}
+            style={{
+              display: "inline-block",
+              margin: "0px 20px",
+            }}
+          />
+          <Silver
+            diameter={180}
+            min={0}
+            max={2}
+            step={1}
+            onValueChange={this.updateChannel}
+            value={this.state.channel}
+            style={{
+              display: "inline-block",
+              margin: "0px 20px",
+            }}
+          />
+        </KnobContainer>
+        <SubmitButton onClick={() => console.log("Clickety Clack")}>
+          SEND
+        </SubmitButton>
       </Radio>
     );
   }
