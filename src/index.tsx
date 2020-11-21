984997bffa2f682fe4f118a7deb3735cf1a1d97c
Visa Pollari <visa.pollari@gmail.com>
2020-11-21 07:49:44 +0200

    use resize box

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { House } from "./House";
-// import ResizeBox from "./resizeBox";
+// import { House } from "./House";
+import ResizeBox from "./resizeBox";
 
-ReactDOM.render(<House />, document.getElementById("root"));
+ReactDOM.render(<ResizeBox />, document.getElementById("root"));
