d790d506f9a4f10c5c72fbd0f5e4c25189ee9858
Visa Pollari <visa.pollari@gmail.com>
2020-11-21 13:08:27 +0200

    Resizer code quality

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { WordTuner } from "./House/Rooms/WordTuner";
 // import { House } from "./House";
-import ResizeBox from "./resizeBox";
+import { ResizeBox } from "./resizeBox";
 
-ReactDOM.render(<ResizeBox />, document.getElementById("root"));
+ReactDOM.render(
+  <ResizeBox width={100} height={200} x={300} y={20} />,
+  document.getElementById("root")
+);
--- src/resizeBox.tsx
+++ src/resizeBox.tsx
@@ -1,53 +1,71 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 
-const ResizableBox = styled.div`
-  background: white;
-  width: 100px;
-  height: 100px;
-  position: absolute;
-  top: 100px;
-  left: 100px;
-`;
-
-const Corners = styled.div`
-  width: 100%;
-  height: 100%;
-  border: 3px solid #4286f4;
-  box-sizing: border-box;
-`;
-
-const Corner = styled.div`
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
-  background: white;
-  border: 3px solid #4286f4;
-  position: absolute;
-`;
-
 const minimumSize = 20;
-let originalWidth;
-let originalHeight;
-let originalX;
-let originalY;
-let originalMouseX;
-let originalMouseY;
+const lastTouch = [0, 0];
 
-export default function ResizeBox() {
+type BoxPorps = {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+};
+
+export const ResizeBox = ({ width, height, x, y }: BoxPorps) => {
+  const originalWidth = width;
+  const originalHeight = height;
+  const originalX = x;
+  const originalY = y;
+
+  const ResizableBox = styled.div`
+    background: white;
+    width: ${originalWidth}px;
+    height: ${originalHeight}px;
+    position: absolute;
+    top: ${originalY}px;
+    left: ${originalX}px;
+  `;
+
+  const Corners = styled.div`
+    width: 100%;
+    height: 100%;
+    border: 3px solid #4286f4;
+    box-sizing: border-box;
+  `;
+
+  const Corner = styled.div`
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background: white;
+    border: 3px solid #4286f4;
+    position: absolute;
+  `;
+
+  const getTouchOffset = (e) => {
+    const ret = [
+      e.touches[0].pageX - lastTouch[0],
+      e.touches[0].pageY - lastTouch[1],
+    ];
+    lastTouch[0] = e.touches[0].clientX;
+    lastTouch[1] = e.touches[0].clientY;
+    return ret;
+  };
+
   useEffect(() => {
     const box = document.querySelector(".resizableBox") as HTMLElement;
     const corners = document.querySelectorAll(".corner");
     for (let i = 0; i < corners.length; i += 1) {
       const corner = corners[i];
+
       const resize = (e) => {
-        const [posX, posY] =
+        const [dX, dY] =
           e.type === "touchmove"
-            ? [e.touches[0].clientX, e.touches[0].clientY]
-            : [e.pageX, e.pageY];
+            ? getTouchOffset(e)
+            : [e.movementX, e.movementY];
         if (corner.classList.contains("bottom-right")) {
-          const width = originalWidth + (posX - originalMouseX);
-          const height = originalHeight + (posY - originalMouseY);
+          const width = box.clientWidth + dX;
+          const height = box.clientHeight + dY;
           if (width > minimumSize) {
             box.style.width = `${width}px`;
           }
@@ -55,70 +73,75 @@ export default function ResizeBox() {
             box.style.height = `${height}px`;
           }
         } else if (corner.classList.contains("bottom-left")) {
-          const height = originalHeight + (posY - originalMouseY);
-          const width = originalWidth - (posX - originalMouseX);
+          const height = box.clientHeight + dY;
+          const width = box.clientWidth - dX;
           if (height > minimumSize) {
             box.style.height = `${height}px`;
           }
           if (width > minimumSize) {
             box.style.width = `${width}px`;
-            box.style.left = `${originalX + (posX - originalMouseX)}px`;
+            box.style.left = `${box.offsetLeft + dX}px`;
           }
         } else if (corner.classList.contains("top-right")) {
-          const width = originalWidth + (posX - originalMouseX);
-          const height = originalHeight - (posY - originalMouseY);
+          const width = box.clientWidth + dX;
+          const height = box.clientHeight - dY;
           if (width > minimumSize) {
             box.style.width = `${width}px`;
           }
           if (height > minimumSize) {
             box.style.height = `${height}px`;
-            box.style.top = `${originalY + (posY - originalMouseY)}px`;
+            box.style.top = `${box.offsetTop + dY}px`;
           }
         } else if (corner.classList.contains("top-left")) {
-          const width = originalWidth - (posX - originalMouseX);
-          const height = originalHeight - (posY - originalMouseY);
+          const width = box.clientWidth - dX;
+          const height = box.clientHeight - dY;
           if (width > minimumSize) {
             box.style.width = `${width}px`;
-            box.style.left = `${originalX + (posX - originalMouseX)}px`;
+            box.style.left = `${box.offsetLeft + dX}px`;
           }
           if (height > minimumSize) {
             box.style.height = `${height}px`;
-            box.style.top = `${originalY + (posY - originalMouseY)}px`;
+            box.style.top = `${box.offsetTop + dY}px`;
           }
         }
       };
-      const stopResize = () => {
-        console.log("soppi");
-        window.removeEventListener("mousemove", resize);
-      };
-      const mouseEvent = (e) => {
-        const [posX, posY] =
-          e.type === "touchstart"
-            ? [e.touches[0].clientX, e.touches[0].clientY]
-            : [e.pageX, e.pageY];
 
+      const mouseEvent = (e) => {
         e.preventDefault();
-        originalWidth = parseFloat(
-          getComputedStyle(box, null)
-            .getPropertyValue("width")
-            .replace("px", "")
-        );
-        originalHeight = parseFloat(
-          getComputedStyle(box, null)
-            .getPropertyValue("height")
-            .replace("px", "")
-        );
-        originalX = box.getBoundingClientRect().left;
-        originalY = box.getBoundingClientRect().top;
-        originalMouseX = posX;
-        originalMouseY = posY;
         window.addEventListener("mousemove", resize);
-        window.addEventListener("mouseup", stopResize);
-        window.addEventListener("touchmove", resize);
-        window.addEventListener("touchend", stopResize);
+        window.addEventListener(
+          "mouseup",
+          () => {
+            window.removeEventListener("mousemove", resize);
+          },
+          { once: true }
+        );
       };
+
+      const touchEvent = (e) => {
+        // const [posX, posY] = [e.touches[0].clientX, e.touches[0].clientY];
+        lastTouch[0] = e.touches[0].clientX;
+        lastTouch[1] = e.touches[0].clientY;
+        e.preventDefault();
+        window.addEventListener("touchmove", resize);
+        window.addEventListener(
+          "touchend",
+          () => {
+            window.removeEventListener("touchmove", resize);
+          },
+          { once: true }
+        );
+        window.addEventListener(
+          "touchcancel",
+          () => {
+            window.removeEventListener("touchmove", resize);
+          },
+          { once: true }
+        );
+      };
+
       corner.addEventListener("mousedown", mouseEvent);
-      corner.addEventListener("touchstart", mouseEvent);
+      corner.addEventListener("touchstart", touchEvent);
     }
 
     // Will be fixed once https://github.com/microsoft/TypeScript-DOM-lib-generator/pull/948 is availeable
@@ -128,11 +151,9 @@ export default function ResizeBox() {
       console.log("Size changed");
       entries.forEach((entry) => {
         const cr = entry.contentRect;
-        /*
         console.log("box:", entry.target);
         console.log(`box size: ${cr.width}px x ${cr.height}px`);
         console.log(`box padding: ${cr.top}px ; ${cr.left}px`);
-        */
         if (cr.width > cr.height) {
           console.log("Ländskeip");
         } else if (cr.width < cr.height) {
@@ -156,4 +177,4 @@ export default function ResizeBox() {
       </Corners>
     </ResizableBox>
   );
-}
+};
