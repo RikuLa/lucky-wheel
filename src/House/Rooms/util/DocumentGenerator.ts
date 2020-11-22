58232f3bc914b2b6f13e3a7e76d094b75293ce6d
Visa Pollari <visa.pollari@gmail.com>
2020-11-22 09:50:11 +0200

    improve code quality

---
+++ src/House/Rooms/util/DocumentGenerator.ts
@@ -0,0 +1,26 @@
+export const generateDocument = (n) => {
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 10);
+  const dd = targetDate.getDate() + n * 3;
+  const mm = targetDate.getMonth() + 1 + (n % 2 === 0 ? 1 : 0) + 1;
+  const yyyy = targetDate.getFullYear() + 231;
+  const dateString = yyyy + "" + dd + "" + mm;
+  const code = "Log" + dateString;
+  console.log(code, yyyy, dd, mm);
+  return {
+    code: code,
+    color: getRandomColor(),
+    solved: false,
+    x: 5 + 10 * Math.round(Math.random() * 7),
+    y: 5 + 5 * Math.round(Math.random() * 17),
+  };
+};
+
+export function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
